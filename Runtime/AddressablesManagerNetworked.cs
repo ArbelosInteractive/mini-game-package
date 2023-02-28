4bf84e2e33b80e8ba117628c67585fee293f1dd1
Kaushal Joshi <kaushal_1995@yahoo.ca>
2023-02-28 15:30:03 -0500

    Update AddressablesManagerNetworked.cs

--- Runtime/AddressablesManagerNetworked.cs
+++ Runtime/AddressablesManagerNetworked.cs
@@ -1,55 +1,55 @@
-using Fusion;
-using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
+// using Fusion;
+// using System;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.AddressableAssets;
+// using UnityEngine.ResourceManagement.AsyncOperations;
 
-namespace Arbelos
-{
-    public class AddressablesManagerNetworked : NetworkBehaviour
-    {
-        public static AddressablesManagerNetworked Instance { get; private set; }
-        private List<AsyncOperationHandle> _asyncOperationHandles = new List<AsyncOperationHandle>();
+// namespace Arbelos
+// {
+//     public class AddressablesManagerNetworked : NetworkBehaviour
+//     {
+//         public static AddressablesManagerNetworked Instance { get; private set; }
+//         private List<AsyncOperationHandle> _asyncOperationHandles = new List<AsyncOperationHandle>();
         
-        public override void Spawned()
-        {
-            if (Instance)
-            {
-                Runner.Despawn(Object);
-            }
-            else
-            {
-                Instance = this;
-                var rootObj = GameObject.Find("------- Managers ----------");
-                transform.parent = rootObj.transform;
-            }
+//         public override void Spawned()
+//         {
+//             if (Instance)
+//             {
+//                 Runner.Despawn(Object);
+//             }
+//             else
+//             {
+//                 Instance = this;
+//                 var rootObj = GameObject.Find("------- Managers ----------");
+//                 transform.parent = rootObj.transform;
+//             }
 
-        }
+//         }
 
-        public void LoadAddressableGameObject(string assetAddress, Action<AsyncOperationHandle<GameObject>> callback)
-        {
-            AsyncOperationHandle<GameObject> opHandle = Addressables.LoadAssetAsync<GameObject>(assetAddress);
-            opHandle.Completed += callback;
-            _asyncOperationHandles.Add(opHandle);
-        }
+//         public void LoadAddressableGameObject(string assetAddress, Action<AsyncOperationHandle<GameObject>> callback)
+//         {
+//             AsyncOperationHandle<GameObject> opHandle = Addressables.LoadAssetAsync<GameObject>(assetAddress);
+//             opHandle.Completed += callback;
+//             _asyncOperationHandles.Add(opHandle);
+//         }
 
-        public void UnloadAddressable(AsyncOperationHandle handle)
-        {
-            _asyncOperationHandles.Remove(handle);
-        }
+//         public void UnloadAddressable(AsyncOperationHandle handle)
+//         {
+//             _asyncOperationHandles.Remove(handle);
+//         }
         
-        private void CleanUp()
-        {
-            foreach (var handle in _asyncOperationHandles)
-            {
-                Addressables.Release(handle);
-            }
-        }
+//         private void CleanUp()
+//         {
+//             foreach (var handle in _asyncOperationHandles)
+//             {
+//                 Addressables.Release(handle);
+//             }
+//         }
 
-        private void OnDestroy()
-        {
-            CleanUp();
-        }
-    }
-}
+//         private void OnDestroy()
+//         {
+//             CleanUp();
+//         }
+//     }
+// }
